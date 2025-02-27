e809dba697602695c03affaa9834b7b298c24c4e
Eduardo <eduardoschiavo09@gmail.com>
2025-02-27 08:56:40 +0100

    solved _01_08

--- src/main/java/arraystring/_01_07_RotateMatrix.java
+++ src/main/java/arraystring/_01_07_RotateMatrix.java
@@ -1,12 +1,31 @@
 package arraystring;
 
+import java.util.Arrays;
+
 /**
  * Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes,
  * write a method to rotate the image by 90 degrees. Can you do this in place?
  */
 class _01_07_RotateMatrix {
     int[][] rotate(int[][] m) {
-        throw new UnsupportedOperationException();
+        if (m.length == 0 || m.length != m[0].length) return m;
+        int n = m.length;
+        for (int layer=0; layer < n/2; layer++){
+            int first = layer; //topmost layer
+            int last = n -1 -layer;
+            for (int i =0; i < n; i++){
+                int offset = i -first;
+                int temp = m[first][i];
+
+                m[first][i] = m[last - offset][first];
+                m[last - offset][first] = m[last][last -offset];
+                m[last][last - offset] = m[i][last];
+                m[i][last] = temp;
+            }
+        }
+        System.out.println(Arrays.deepToString(m));
+        return m;
+
     }
 
 
--- src/main/java/arraystring/_01_08_ZeroMatrix.java
+++ src/main/java/arraystring/_01_08_ZeroMatrix.java
@@ -1,10 +1,41 @@
 package arraystring;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column are set to 0.
  */
 class _01_08_ZeroMatrix {
     int[][] zero(int[][] matrix) {
-        throw new UnsupportedOperationException();
+        ArrayList<Integer> zCols = new ArrayList<Integer>();
+        ArrayList<Integer> zRows = new ArrayList<Integer>();
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        //find zeros
+        for (int c=0; c< n; c++){
+            for (int r=0; r<m; r++){
+                if(matrix[c][r] == 0){
+                    zCols.add(c);
+                    zRows.add(r);
+                }
+            }
+        }
+
+        for(int c: zCols){
+            for (int i =0; i<m; i++) {
+                matrix[c][i] = 0;
+            }
+        }
+
+        for(int r: zRows){
+            for (int i =0; i<n; i++){
+                matrix[i][r] =0;
+            }
+        }
+
+        return matrix;
+
     }
 }

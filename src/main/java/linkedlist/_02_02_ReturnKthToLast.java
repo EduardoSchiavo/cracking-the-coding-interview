bf5efef683a2d844b748725479eed4f1fbd2801e
Eduardo <eduardoschiavo09@gmail.com>
2025-03-13 08:29:01 +0100

    _02_02  solved

--- src/main/java/linkedlist/_02_02_ReturnKthToLast.java
+++ src/main/java/linkedlist/_02_02_ReturnKthToLast.java
@@ -6,6 +6,22 @@ package linkedlist;
 class _02_02_ReturnKthToLast {
 
     LinkedListNode kthToLast(LinkedListNode head, int k) {
-        throw new UnsupportedOperationException();
+        if (head == null || head.next == null){
+            return head;
+        }
+        LinkedListNode p1 = head;
+        LinkedListNode p2 = head;
+        for (int i =0; i< k; i++){
+            if (p1.next == null){
+                return null;
+            }
+            p1=p1.next;
+        }
+        while(p1.next != null){
+            p1=p1.next;
+            p2=p2.next;
+        }
+        return p2;
+
     }
 }

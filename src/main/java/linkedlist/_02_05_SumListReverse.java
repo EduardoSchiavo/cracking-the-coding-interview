70274735eb8925e3ca00d4d7cd4570a3f026f468
Eduardo <eduardoschiavo09@gmail.com>
2025-05-16 12:21:57 +0200

    _02_05_ solved

--- src/main/java/linkedlist/_02_05_SumListReverse.java
+++ src/main/java/linkedlist/_02_05_SumListReverse.java
@@ -1,5 +1,6 @@
 package linkedlist;
 
+
 /**
  * Sum Lists: You have two numbers represented by a linked list,
  * where each node contains a single digit.
@@ -12,6 +13,44 @@ package linkedlist;
  */
 class _02_05_SumListReverse {
     LinkedListNode sum(LinkedListNode l1, LinkedListNode l2) {
-        throw new UnsupportedOperationException();
+        if (l1 == null && l2 == null){
+            return null;
+        }
+
+        LinkedListNode resH = null;
+        LinkedListNode resT = null;
+        LinkedListNode c1 = l1;
+        LinkedListNode c2 = l2;
+        int[] resAndCarry = new int[]{0, 0};
+
+        while ( c1 != null || c2 != null){
+            int n1 = (c1 != null) ? c1.val : 0;
+            int n2 = (c2 != null) ? c2.val : 0;
+            resAndCarry = addNumbers(n1,n2, resAndCarry[1]);
+            if (resH == null){
+                resH = new LinkedListNode(resAndCarry[0]);
+                resT = resH;
+            } else {
+                resT.next = new LinkedListNode(resAndCarry[0]);
+                resT = resT.next;
+            }
+            c1 = (c1 != null) ? c1.next : null;
+            c2 = (c2 != null) ? c2.next : null;
+        }
+        if (resAndCarry[1] == 1){
+            resT.next = new LinkedListNode(1);
+        }
+
+        return resH;
+
+
+
     }
+
+    private int[] addNumbers(int a, int b, int r){
+        int res = a + b + r;
+        int carry = (res>9) ? 1 : 0;
+        return new int[]{res%10, carry};
+    }
+
 }

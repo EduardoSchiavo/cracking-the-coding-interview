8ceadf9da19d1711f24f2639c8c3dcf522fea114
Eduardo <eduardoschiavo09@gmail.com>
2025-05-23 14:36:57 +0200

    _02_05_ solved the dumb way

--- src/main/java/linkedlist/_02_05_SumList.java
+++ src/main/java/linkedlist/_02_05_SumList.java
@@ -13,7 +13,68 @@ package linkedlist;
 class _02_05_SumList {
 
     LinkedListNode sum(LinkedListNode l1, LinkedListNode l2) {
-        throw new UnsupportedOperationException();
+        System.out.println("before l1, l2 " + l1 + l2);
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+        System.out.println("after l1, l2 " + l1 + l2);
+
+        if (l1 == null && l2 == null){
+            return null;
+        }
+
+        LinkedListNode resH = null;
+        LinkedListNode resT = null;
+        LinkedListNode c1 = l1;
+        LinkedListNode c2 = l2;
+        int[] resAndCarry = new int[]{0, 0};
+
+        while ( c1 != null || c2 != null){
+            int n1 = (c1 != null) ? c1.val : 0;
+            int n2 = (c2 != null) ? c2.val : 0;
+            resAndCarry = addNumbers(n1,n2, resAndCarry[1]);
+            if (resH == null){
+                resH = new LinkedListNode(resAndCarry[0]);
+                resT = resH;
+            } else {
+                resT.next = new LinkedListNode(resAndCarry[0]);
+                resT = resT.next;
+            }
+            c1 = (c1 != null) ? c1.next : null;
+            c2 = (c2 != null) ? c2.next : null;
+        }
+        if (resAndCarry[1] == 1){
+            resT.next = new LinkedListNode(1);
+        }
+
+        return reverseList(resH);
+        }
+
+    private LinkedListNode reverseList(LinkedListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+
+        LinkedListNode curr = head;
+        LinkedListNode prev = null;
+
+        while (curr.next != null) {
+            LinkedListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        curr.next = prev;
+        head = curr;
+        return head;
+    }
+    private int[] addNumbers(int a, int b, int r){
+        int res = a + b + r;
+        int carry = (res>9) ? 1 : 0;
+        return new int[]{res%10, carry};
     }
 
-}
+    }
+
+
+
+

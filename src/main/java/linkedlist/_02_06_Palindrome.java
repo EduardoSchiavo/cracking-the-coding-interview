c50cffb7cb555647fb33634d944257521ff4da66
Eduardo <eduardoschiavo09@gmail.com>
2025-05-23 15:14:46 +0200

    _02_06_ solved the extremely dumb way

--- src/main/java/linkedlist/_02_06_Palindrome.java
+++ src/main/java/linkedlist/_02_06_Palindrome.java
@@ -6,8 +6,52 @@ package linkedlist;
 class _02_06_Palindrome {
 
     boolean isPalindrome(LinkedListNode head) {
-        throw new UnsupportedOperationException();
+        LinkedListNode previous = deepCopyList(head);
+        LinkedListNode rev = reverseList(head);
+        return areEqual(previous, rev);
+
+    }
+
+    private LinkedListNode deepCopyList(LinkedListNode head){
+        if (head == null){ return null;}
+
+        LinkedListNode newHead = new LinkedListNode(head.val);
+        LinkedListNode curr = head;
+        LinkedListNode newCurr = newHead;
+        while(curr.next != null){
+            newCurr.next = new LinkedListNode(curr.next.val);
+            curr = curr.next;
+            newCurr = newCurr.next;
+        }
+        return newHead;
+    }
+
+    private LinkedListNode reverseList(LinkedListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+
+        LinkedListNode curr = head;
+        LinkedListNode prev = null;
+
+        while (curr.next != null) {
+            LinkedListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        curr.next = prev;
+
+        return curr;
     }
 
 
+    private boolean areEqual(LinkedListNode a, LinkedListNode b) {
+        while (a != null && b != null) {
+            if (a.val != b.val) return false;
+            a = a.next;
+            b = b.next;
+        }
+        return a == null && b == null;
+    }
 }
